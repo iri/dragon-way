8f17a40381dde617c298f0593dd1acfbcd8949ad
Ingars Ribners <ram3a12@gmail.com>
2024-02-04 18:42:03 +0300

    Single page with local storage synchronization

---
+++ view.view.css.ts
@@ -0,0 +1,11 @@
+namespace $ {
+	$mol_style_define( $my_wiki, {
+		color: 'red',
+
+		Body: {
+			padding: $mol_gap.block
+		}
+
+	} )
+
+}
---
+++ view.view.ts
@@ -0,0 +1,10 @@
+namespace $.$$ {
+	
+	export class $my_wiki extends $.$my_wiki {
+
+		text( next?: string ) {
+			return this.$.$mol_state_local.value( 'text', next ) ?? ''
+		}
+
+	}
+}
